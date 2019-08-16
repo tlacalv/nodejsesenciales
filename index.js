60a8d64b5b78a80e2b725d83672eccfb9c19860b
tlacalv <radeon777@live.com>
2019-08-16 17:36:54 -0600

    Using Express JS

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res)=>{
-    res.send("Hola, estoy en la ruta '/'!");
-});
+require("./routes/views")(app);
+require("./routes/special")(app);
 
 console.log("Iniciando Express.js");
 app.listen(3000, ()=>{
--- routes/views/home.js
+++ routes/views/home.js
@@ -2,4 +2,10 @@ module.exports = function (app) {
     app.get("/", (req, res)=>{
         res.send("Hola, estoy en la ruta '/'!");
     });
+    app.get("/leon", (req, res)=>{
+        res.send("Hola, estoy en la ruta '/leon'!");
+    });
+    app.post("/leon", (req, res)=>{
+        res.send("Hola, estoy en el post '/leon'!");
+    });
 }
