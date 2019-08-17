6a0d390809dcd26d63b2e2c390bd1d699631cf4d
tlacalv <radeon777@live.com>
2019-08-17 11:32:24 -0600

    Using middlewares

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
-const app = express();
+const logger = require("morgan")//Es un logger, es un middleware, se encarga de loguear las peticiones en la consola
+const bodyParser = require("body-parser")//hace parse de ciertos request
 
+
+const app = express();
+app.use(logger((tokens, req,res)=>{
+    let val = tokens.url(req,res)
+    val =val.split("/")
+    val = val[val.length-1]
+    if(val ==='leon'){
+        console.log('Eres leon')
+    }
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms'
+    ].join('--')
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))//no queremos la verson extendida del middleware
 require("./routes/views")(app);
 require("./routes/special")(app);
 
