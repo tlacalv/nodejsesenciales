8496a587c44881d731e9701958e8a3e47695ed79
tlacalv <radeon777@live.com>
2019-08-17 11:40:53 -0600

    creating a JSON

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use(logger("dev"));
-app.use(bodyParser.json());
+app.use( logger("dev") );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: false }) );
 
-require("./routes")(app);
+require("./routes/views")(app);
+require("./routes/special")(app);
 
-app.get("*", (req, res)=>{
-    res.send("Hola, soy Express!");
-});
-
+console.log("Iniciando Express.js");
 app.listen(3000, ()=>{
     console.log("Express ha iniciado correctamente!");
 });
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const glob = require("glob");
-const path = require("path");
-
-module.exports = function(app){
-  glob.sync("./routes/**/*.js", { ignore: "./routes/**/index.js" }).forEach((file)=>{
-    require(path.resolve(file))(app);
-  })
-}
