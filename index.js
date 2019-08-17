80ce848049cde3c747c8a79931b26d3c4691986b
tlacalv <radeon777@live.com>
2019-08-17 11:37:16 -0600

    Using middlewares

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 const express = require("express");
-const logger = require("morgan")//Es un logger, es un middleware, se encarga de loguear las peticiones en la consola
-const bodyParser = require("body-parser")//hace parse de ciertos request
-
-
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(logger((tokens, req,res)=>{
-    let val = tokens.url(req,res)
-    val =val.split("/")
-    val = val[val.length-1]
-    if(val ==='leon'){
-        console.log('Eres leon')
-    }
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms'
-    ].join('--')
-}))
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))//no queremos la verson extendida del middleware
-require("./routes/views")(app);
-require("./routes/special")(app);
+app.use(logger("dev"));
+app.use(bodyParser.json());
 
-console.log("Iniciando Express.js");
+require("./routes")(app);
+
+app.get("*", (req, res)=>{
+    res.send("Hola, soy Express!");
+});
+
 app.listen(3000, ()=>{
     console.log("Express ha iniciado correctamente!");
 });
